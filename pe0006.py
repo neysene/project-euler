cc9f35044eb273a3d37d4e93ebd6e31a085d1999
neysene <ismailgonul@gmail.com>
2016-01-08 21:00:52 +0200

    init

---
+++ pe0006.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    s = 0
+    for i in xrange(101):
+        s += i**2  
+    print ((100+1)*50)**2 - s
---
+++ pe0007.py
@@ -0,0 +1,10 @@
+def primelist(n):
+    sieve = [True] * (n/2)
+    for i in xrange(3,int(n**0.5)+1,2):
+        if sieve[i/2]:
+            sieve[i*i/2::i] = [False] * ((n-i*i-1)/(2*i)+1)
+    return [2] + [2*i+1 for i in xrange(1,n/2) if sieve[i]]
+
+if __name__ == '__main__':
+    p = primelist(150000)
+    print p[10000]
