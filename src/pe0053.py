fd09e4ea183b3dbcf5428ce359bfac840c3da3fa
neysene <ismailgonul@gmail.com>
2016-01-26 14:04:02 +0200

    init

---
+++ src/pe0053.py
@@ -0,0 +1,16 @@
+def ret_count(a):
+    n, d, t, k = a, 1, a-1, 2
+    while k < t:
+        if n/d > 1000000:
+            break
+        n *= t
+        t -= 1
+        d *= k
+        k += 1
+    return t-k+2 if k<t else 0
+
+if __name__ == '__main__':
+    sm = 0
+    for i in xrange(23,101):
+        sm += ret_count(i)
+    print sm
