47b2b9bd6e41cb975d61bdf9627dfd88f481c43f
neysene <ismailgonul@gmail.com>
2016-01-27 09:23:11 +0200

    modi

--- src/pe0053.py
+++ src/pe0053.py
@@ -1,13 +1,13 @@
 def ret_count(a):
     n, d, t, k = a, 1, a-1, 2
-    while k < t:
+    while True:
         if n/d > 1000000:
             break
         n *= t
         t -= 1
         d *= k
         k += 1
-    return t-k+2 if k<t else 0
+    return t-k+2
 
 if __name__ == '__main__':
     sm = 0
